2da3f8dc1de382ef52e13c13460528581519f93c
Philippe Emanuel Almeida Corrêa <philippe.correa@basisti.com.br>
2024-10-27 04:10:03 -0300

    Create Abstraction on MediatR for CQRS - Command and Query separation

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommandHandler<in TComand>
+        : ICommandHandler<TComand, Unit>
+        where TComand : ICommand<Unit>        
+    {
+    }
+
+    public interface ICommandHandler<in TComand, TResponse> 
+        : IRequestHandler<TComand, TResponse>
+        where TComand : ICommand<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
