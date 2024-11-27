6ed431dd26192eff6374a6c17a250f95bfd856b7
Philippe Emanuel Almeida Corrêa <philippe.correa@basisti.com.br>
2024-11-27 12:02:06 -0300

    alteracoes

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Guid) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+
+    internal class GetProductByIdQuerydHandler(IDocumentSession session, ILogger<GetProductByIdQuerydHandler> logger) 
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Guid, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(product);
+        }
+    }
+}
